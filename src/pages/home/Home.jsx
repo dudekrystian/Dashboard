7cb6c2ee3e0c1a3a7445a2ab4777079da7029011
dudekrystian <dudekrystian@gmail.com>
2023-08-14 12:11:36 +0200

    create basic structure pages with files

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./home.scss";
+
+export const Home = () => {
+  return (
+    <div className='home'>
+        <h1 className='title'>Panel Dashboard</h1>
+    </div>
+  )
+}
