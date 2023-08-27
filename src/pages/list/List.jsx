d067f3559115bd38eae1d11629da2e31b0143e6c
dudekrystian <dudekrystian@gmail.com>
2023-08-27 16:53:32 +0200

    create component for list Items

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
 import "./list.scss";
+import Sidebar  from "../../components/sidebar/Sidebar";
+import Navbar from '../../components/navbar/Navbar';
+import Datatable from '../../components/datatable/Datatable';
 
-export const List = () => {
+ const List = () => {
   return (
-    <div className='list'>List</div>
+    <div className='list'>
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
   )
-}
+};
+
+export default List;
